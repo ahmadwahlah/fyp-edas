9e714a92aec9f8db6afbe63ccaaf6941db939482
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-02-26 16:47:53 +0500

    Port is changed to 4000 instead of 3000

--- router/auth.js
+++ router/auth.js
@@ -8,7 +8,7 @@ router.get('/',(req,res)=> {
         if(err){
             console.log(err.message);
         }else {
-            // res.send(result);
+            console.log(result);
             res.send("Hello I am working");
         }
     });
