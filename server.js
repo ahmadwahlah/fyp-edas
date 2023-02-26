0c83523686b80571bec723fea1f033cc730d497b
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-02-26 16:27:07 +0500

    port is defined

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 dotenv.config({path: './config.env'});
 require("./database");
 require('./router/auth');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 //link the router file 
